5b46155a9d2008369d928cfbc29e0cf0937b106c
Denis Michailov <denis.michailov@gmail.com>
2016-10-07 14:45:47 +1000

    can get input json data now. calculating approval rate. make all 4 category

--- src/js/components/donut-chart.js
+++ src/js/components/donut-chart.js
@@ -2,104 +2,124 @@ var d3 = require("d3");
 
 module.exports = (function(){
 
-    var el;
-    var $el;
-    var w = 0, h = 0;
-    var color; // color scaling
+	var el;
+	var $el;
+	var w = 0, h = 0;
 
-    var tooltip = d3.select('#tooltip');
+	var tooltip = d3.select('#tooltip');
 
-    var init = function(element, data, options){
-        // инициализируем стартовые перменные
-        el = element;
-        $el = document.querySelector(el);
-        w = $el.clientWidth;
-        h = $el.clientHeight;
+	var init = function(element, data, options, type){
+		// init start variables
+		el = element;
+		$el = document.querySelector(el);
+		w = $el.clientWidth;
+		h = $el.clientHeight;
 
-        // DEFAULT VALUES
-        var opt = {};
-        opt.colors   = options.colors || ['#1ABC9C', '#F77824', '#E69A21'];
-        opt.bgColor  = options.bgColor || "#F5F8FD";
-        opt.stroke   = options.stroke || 16;
-        opt.rFactor  = options.rFactor || 0.4;
-        opt.bgFactor = options.bgFactor || 0.3
+		// DEFAULT VALUES
+		var opt = {};
+		opt.colors   = {
+			all: {
+				payment: "#1abc9c",
+				refund: "#f77824",
+				payout: "#e69a21",
+				declined: options.bgColor || "#F5F8FD"
+			},
+			payment: {
+				captured: "#1abc9c",
+				declined: "#055c8d",
+				voided: "#d8e1ea",
+				chargeback: "#be4727"
+			},
+			refund: {
+				captured: "#f77824",
+				declined: "#055c8d"
+			},
+			payout: {
+				captured: "#e69a21",
+				declined: "#055c8d"
+			}
+		};
 
-        var innerRadius = w*opt.rFactor - opt.stroke;
-        var outerRadius = w*opt.rFactor;
+		opt.bgColor  = options.bgColor || "#F5F8FD";
+		opt.stroke   = options.stroke || 16;
+		opt.rFactor  = options.rFactor || 0.4;
 
-        color = d3.scale.ordinal()
-            .range(opt.colors);
+		var innerRadius = w*opt.rFactor - opt.stroke;
+		var outerRadius = w*opt.rFactor;
 
-        // main svg
-        var svg = d3.select(el).append('svg')
-            .attr('width', w)
-            .attr('height', h);
+		// main svg
+		var svg = d3.select(el).append('svg')
+			.attr('width', w)
+			.attr('height', h);
 
-        // main group
-        var groupDonut = svg.append('g')
-            .attr('transform', 'translate(' + w/2 + ','+ h/2 +')');
+		// main group
+		var groupDonut = svg.append('g')
+			.attr('transform', 'translate(' + w/2 + ','+ h/2 +')');
 
-        // constructors
-        var arc = d3.svg.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(outerRadius);
+		// constructors
+		var arc = d3.svg.arc()
+			.innerRadius(innerRadius)
+			.outerRadius(outerRadius);
 
-        var pie = d3.layout.pie()
-            .sort(null)
-            .startAngle(1.1*Math.PI)
-            .endAngle(3.1*Math.PI)
-            .value(function(d) { return d.value; });
+		var dataIn = [];
+		for(var key in data){
+			dataIn.push({
+				name: key,
+				value: data[key]
+			})
+		}
 
-        var arcs = groupDonut.selectAll('.arc')
-            .data(pie(data))
-            .enter()
-            .append('g')
-            .attr('class', 'arc')
-            .attr('stroke-width', opt.stroke/2)
-            .attr('stroke', opt.bgColor)
-            ;
+		var pie = d3.layout.pie()
+			.sort(null)
+			.startAngle(1.1*Math.PI)
+			.endAngle(3.1*Math.PI)
+			.value(function(d) {
+				return d.value;
+			});
 
-        var item = arcs.append('path')
-            .attr('class', 'arc-interactive')
-            .attr('fill', d => {
-                return color(d.data.value)
-            })
-            .transition()
-            .delay(function(d, i) { return i * 300; })
-            .duration(500)
-            .ease('cubic')
-            .attrTween('d', function(d) {
-                var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
-                return function(t) {
-                    d.endAngle = i(t);
-                    return arc(d);
-                };
-            });
+		var arcs = groupDonut.selectAll('.arc')
+			.data(pie(dataIn))
+			.enter()
+			.append('g')
+			.attr('class', 'arc')
+			.attr('stroke-width', opt.stroke/4)
+			.attr('stroke', opt.bgColor)
+			;
 
-            // events
-            arcs
-            .on('mouseout', tooltipHide)
-            .on('click', function(d){
-                var x = d3.event.offsetX;
-                var y = d3.event.offsetY;
-                tooltip.text(d.value);
-                x = x - (tooltip[0][0].clientWidth/2);
-                y = y - (tooltip[0][0].clientHeight + 8);
-                tooltip.style('left', x + 'px')
-                    .style('top', y + 'px');
-                tooltipShow();
-            });
-    }; // endof function init
+		var item = arcs.append('path')
+			.attr('fill', function(d) {
+				return opt.colors[type][d.data.name];
+			})
+			.transition()
+			.delay(function(d, i) { return i * 100; })
+			.duration(300)
+			.ease('cubic')
+			.attrTween('d', function(d) {
+				var i = d3.interpolate(d.startAngle+0.1, d.endAngle);
+				return function(t) {
+					d.endAngle = i(t);
+					return arc(d);
+				};
+			});
 
-    var tooltipShow = function(){
-        tooltip.style('opacity', 1);
-    };
-    var tooltipHide = function(){
-        tooltip.style('opacity', 0);
-    };
+		// init app-rate value
+		var total = 0;
 
-    return {
-        init: init,
-        tooltipHide: tooltipHide
-    };
+		Object.keys(data).map(function(key){
+			total += data[key];
+		});
+
+		if(type != 'all') {
+			var x = data.captured/total*100;
+			$el.querySelector('.app-rate-value').innerHTML = x.toFixed(2) + "%";
+		} else {
+			var x = (data.payment + data.refund + data.payout)/total*100;
+			$el.querySelector('.app-rate-value').innerHTML = x.toFixed(2) + "%";
+		}
+
+	}; // endof function init
+
+	return {
+		init: init
+	};
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,35 @@
-// var d3 = require("d3");
-// var classie = require("./classie");
-// var settings = require("./settings");
-//
-// console.log( "d3 version is " + d3.version );
+var d3 = require("d3");
 var donutChart = require("./components/donut-chart");
 
-var data = [ 12397, 4882, 771 ];
+// ++++++++++++++++++
 
-var data = [
-    {
-        name: "payments",
-        value: 12379
-    },
-    {
-        name: "refunds",
-        value: 4882
-    },
-    {
-        name: "payouts",
-        value: 771
-    }
-];
+var data = {};
+var elements = {
+	all: ".donut-chart.all",
+	payment: ".donut-chart.payment",
+	refund: ".donut-chart.refund",
+	payout: ".donut-chart.payout"
+};
 
 var options = {
-    colors: ['#1ABC9C', '#F77824', '#E69A21'],
-    bgColor: "#F5F8FD",
-    stroke: 16,
-    rFactor: 0.4,
-    bgFactor: 0.5
+	bgColor: "#F5F8FD",
+	stroke: 8,
+	rFactor: 0.41
 };
-donutChart.init("#donut-chart", data, options);
+
+var jsonHandler = function(json){
+	Object.keys(json).map(function(f){
+		data[f] = json[f];
+		data.all = {
+			payment: json.payment.captured,
+			refund: json.refund.captured,
+			payout: json.payout.captured,
+			declined: json.payment.declined + json.refund.declined + json.payout.declined + json.payment.voided + json.payment.chargeback
+		}
+	});
+	for (var key in data) {
+		donutChart.init(elements[key], data[key], options, key);
+	}
+};
+
+d3.json('/data/input.json', jsonHandler)
